113be412f401b403dc305b5270858b9bf44da61f
Sarvesh Sambhaji Gawade <sarveshgawadesg@gmail.com>
2023-10-23 15:14:45 +0530

    Added a samll project on Props

---
+++ 03_Hooks_TailwindBasics/src/App.jsx
@@ -0,0 +1,16 @@
+import Card from './Components/Card'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Card btnText="Click here !" userName="Lambo" />
+
+      <Card btnText="Visit" userName="Ferrari" />
+    </>
+  )
+}
+
+export default App
