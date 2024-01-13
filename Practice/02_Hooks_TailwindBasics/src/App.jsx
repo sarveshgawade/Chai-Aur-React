63f8d46bfe621cd348732ff80695a22e682bc7e8
Sarvesh Sambhaji Gawade <sarveshgawadesg@gmail.com>
2024-01-13 15:41:06 +0530

    practice 02

---
+++ Practice/02_Hooks_TailwindBasics/src/App.jsx
@@ -0,0 +1,18 @@
+import Card from './components/Card'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Card btnTxt="Open" username="Ajay" imgSrc= "https://t4.ftcdn.net/jpg/01/77/47/67/360_F_177476718_VWfYMWCzK32bfPI308wZljGHvAUYSJcn.jpg"></Card>
+
+      <br /><br />
+      
+      <Card btnTxt="Close" username="Raj" imgSrc="https://t3.ftcdn.net/jpg/05/63/70/98/360_F_563709848_OdMVkfRpOZbSsy6bFBkthhJzleFxM7Cn.jpg"></Card>
+    </>
+  )
+}
+
+export default App
